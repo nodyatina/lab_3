8c3cb39d910b237e4f39c5a1cf32787bfe06dfe2
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-08 12:44:27 +0300

    Update Album.cs

--- Album.cs
+++ Album.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 namespace MusicGroup
 {
     public class Album
-    {
-        public List<Song> SongList { get; set; }
+    {public class Album
+{
+    public List<Song> SongList { get; set; }
 
-        public Album(List<Song> songs)
-        {
+    public Album(List<Song> songs)
+    {
         if (songs == null)
         {
-        throw new ArgumentNullException("нулл отстой");
+            throw new ArgumentNullException("нулл отстой");
+        }
+
+        foreach (Song song in songs)
+        {
+            if (song == null)
+            {
+                throw new ArgumentException("Один из элементов списка Song равен null");
+            }
         }
 
         SongList = songs;
-        }
-
+    }
+}
     }
 
 }
